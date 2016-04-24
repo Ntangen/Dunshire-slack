67458ab659999664e77611da672232f020b2eee7
Blair Reeves <gaslight@gmail.com>
2016-04-23 20:14:38 -0400

    sat

--- index.js
+++ index.js
@@ -70,34 +70,29 @@ if (process.env.MONGOLAB_URI) {
 
 // initialization
 
-if (process.env.TOKEN || process.env.SLACK_TOKEN) {
-    //Treat this as a custom integration
-    console.log("this is a custom integration");
-    var customIntegration = require('./lib/custom_integrations');
-    var token = (process.env.TOKEN) ? process.env.TOKEN : process.env.SLACK_TOKEN;
-    var controller = customIntegration.configure(token, config, onInstallation);
-} else if (process.env.CLIENT_ID && process.env.CLIENT_SECRET && process.env.PORT) {
-    //Treat this as an app
-    var app = require('./lib/apps');
-    var controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, onInstallation);
-} else {
-    console.log('Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment');
-    process.exit(1);
-}
+// if (process.env.TOKEN || process.env.SLACK_TOKEN) {
+//     //Treat this as a custom integration
+//     console.log("this is a custom integration");
+//     var customIntegration = require('./lib/custom_integrations');
+//     var token = (process.env.TOKEN) ? process.env.TOKEN : process.env.SLACK_TOKEN;
+//     var controller = customIntegration.configure(token, config, onInstallation);
+// } else if (process.env.CLIENT_ID && process.env.CLIENT_SECRET && process.env.PORT) {
+//     //Treat this as an app
+//     var app = require('./lib/apps');
+//     var controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, onInstallation);
+// } else {
+//     console.log('Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment');
+//     process.exit(1);
+// }
 
-// var controller = Botkit.slackbot({
-//   var BotkitStorage = require('botkit-storage-mongo');
-//     config = {
-//         storage: BotkitStorage({mongoUri: process.env.MONGOLAB_URI}),
-//     };
-// }).configureSlackApp(
-//   {
-//     clientId: process.env.clientId,
-//     clientSecret: process.env.clientSecret,
-//     scopes: ['bot'],
-//   }
-// );
-
+var controller = Botkit.slackbot(
+    process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, onInstallation).configureSlackApp(
+    {
+        clientId: process.env.clientId,
+        clientSecret: process.env.clientSecret,
+        scopes: ['bot'],
+    }
+);
 
 // var http = require('http');
 // http.createServer(function (req, res) {
