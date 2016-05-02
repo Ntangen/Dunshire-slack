36d3f716a81f70a32e0e768b3c36202f387d1809
Blair <gaslight@gmail.com>
2016-05-02 16:04:35 -0400

    user fix

--- utility.js
+++ utility.js
@@ -221,8 +221,8 @@ module.exports = {
 				// pick up event
 				var temp3 = sessionevents.minor[temp2]
 				console.log("temp3: " + temp3);
-				console.log(events.eventReturner[temp3]);
-		        temp += events.eventReturner[temp3];
+				console.log(events.eventReturner(temp3));
+		        temp += events.eventReturner(temp3);
 			}
 		}
 		sessionevents.tobesaved += temp;
